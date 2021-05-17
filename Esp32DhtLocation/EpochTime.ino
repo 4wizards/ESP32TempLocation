73f67bb3ee5e61ced276cf62debf9bf10590951b
Vincent <avipami@gmail.com>
2021-05-17 19:47:05 +0200

    Create Functions For ESP32

---
+++ Esp32DhtLocation/EpochTime.ino
@@ -0,0 +1,17 @@
+
+void EpochTimeInit()
+{
+  configTime(3600, 0, "pool.ntp.org", "time.nist.gov");
+  while (true)
+    {
+      epochTime = time(NULL);
+
+      if (epochTime == 28800) {
+        delay(2000);
+      }
+      else
+      {
+        break;
+      }
+    }
+}
---
+++ Esp32DhtLocation/WiFiConnectFunction.ino
@@ -0,0 +1,4 @@
+#include config.h;
+
+// Wifi Hotspot connection settings and functions
+
