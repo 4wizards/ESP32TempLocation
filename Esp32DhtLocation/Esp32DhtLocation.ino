d362a7ba381385d53a99f41c19df2b4c793448ff
Vincent <avipami@gmail.com>
2021-05-17 19:45:33 +0200

    Main file create

---
+++ Esp32DhtLocation/Esp32DhtLocation.ino
@@ -0,0 +1,13 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+
+}
+
+void loop() {
+  Serial.println(WIFI_HOTSPOTNAME);
+  // put your main code here, to run repeatedly:
+
+}
